154090e119d0953514b6aeaa8fccaf75dfcfd9f8
Logan Wood <lwood3499@gmail.com>
2019-06-24 21:28:15 -0500

    file structure

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Home View</h1>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>portfolio/Stock vue</h1>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+</style>
